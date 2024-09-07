bae300850861489da3b0c58fa32fef2bda04cec6
Tristán Nores <45902437+Nores33@users.noreply.github.com>
2024-09-07 14:12:03 -0300

    Added Shader class
    The same shader class from learnopengl.com and tested it by drawing a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "graphics/Renderer.hpp"
 #include "graphics/opengl/OpenGLRenderer.hpp"
+#include "graphics/Shader/Shader.hpp"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -22,9 +23,41 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // TRIANGLE
+    /* ------------------------------------------------------------------------ */
+    Shader shader("vertex_shader.vs", "fragment_shader.fs");
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    /* ------------------------------------------------------------------------ */
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
         renderer->render();
+
+        // Render Triangle
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
